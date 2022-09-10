5058c618457fdbbf09e2648c24619b0f25bb8b7f
ongkay <bisnismilyarandollar@gmail.com>
2022-09-10 21:17:53 +0700

    updatelagi

--- oret.js
+++ oret.js
@@ -1,9 +1,13 @@
+/* eslint-disable arrow-body-style */
+// eslint-disable-next-line spaced-comment
+//format buat plugin
+// eslint-disable-next-line no-unused-vars
 const notesPlugin = {
   name: 'notes',
   version: '1.0.0',
   register: async (server, options) => {
     // contoh, menetapkan routing untuk /notes
-    const notes = options.notes;
+    const { notes } = options;
     server.route([
       {
         method: 'GET',
