d31a23dd86f073c75c10ab63b64b1d9098d5aa4a
ongkay <bisnismilyarandollar@gmail.com>
2022-09-16 22:13:11 +0700

    authorization implemented

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -3,6 +3,7 @@ const { nanoid } = require('nanoid')
 const InvariantError = require('../../exceptions/InvariantError')
 const { mapDBToModel } = require('../../utils')
 const NotFoundError = require('../../exceptions/NotFoundError')
+const AuthorizationError = require('../../exceptions/AuthorizationError')
 
 class NotesService {
   constructor() {
@@ -10,15 +11,15 @@ class NotesService {
   }
 
   // [1] BUAT FUNGSI ADDNOTE :
-  async addNote({ title, body, tags }) {
+  async addNote({ title, body, tags, owner }) {
     const id = nanoid(16)
     const createdAt = new Date().toISOString()
     const updatedAt = createdAt
 
     // -buat objek query untuk memasukan notes baru ke database
     const query = {
-      text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
-      values: [id, title, body, tags, createdAt, updatedAt],
+      text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id',
+      values: [id, title, body, tags, createdAt, updatedAt, owner],
     }
 
     // -mengeksekusi query yang sudah dibuat, akan berjalan secara asynchronous
@@ -35,9 +36,13 @@ class NotesService {
   }
 
   // [2] Membuat Fungsi getNotes
-  // - Di dalamnya kita dapatkan seluruh data notes yang ada di database dgn query : “SELECT * FROM notes”
-  async getNotes() {
-    const result = await this._pool.query('SELECT * FROM notes')
+  // - fungsi getNotes tidak akan mengembalikan seluruh catatan yang disimpan pada tabel notes. Melainkan hanya catatan yang dimiliki oleh owner saja.
+  async getNotes(owner) {
+    const query = {
+      text: 'SELECT * FROM notes WHERE owner = $1',
+      values: [owner],
+    }
+    const result = await this._pool.query(query)
     return result.rows.map(mapDBToModel)
   }
 
@@ -89,6 +94,21 @@ class NotesService {
 
     if (!result.rows.length) {
       throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan')
+    }
+  }
+
+  async verifyNoteOwner(id, owner) {
+    const query = {
+      text: 'SELECT * FROM notes WHERE id = $1',
+      values: [id],
+    }
+    const result = await this._pool.query(query)
+    if (!result.rows.length) {
+      throw new NotFoundError('Catatan tidak ditemukan')
+    }
+    const note = result.rows[0]
+    if (note.owner !== owner) {
+      throw new AuthorizationError('Anda tidak berhak mengakses resource ini')
     }
   }
 }
